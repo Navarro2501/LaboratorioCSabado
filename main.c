a5242394ca8dc647e163323e3d8e726d594e15e7
Fernando Navarro <kevin.navarrogrz@gmail.com>
2019-08-25 16:52:05 -0500

    Agregado de la resolución de la segunda ecuación

--- main.c
+++ main.c
@@ -7,9 +7,10 @@ Grupo: 008
 Horario: Sábados 09:00 - 11:00
 Matrícula: 1618122
 */
+//Segunda modificación agregando la resolución a la segunda ecuación
 
 int main(int argc, char *argv[]) {
-	//Identificación del branch práctica 3
+	//Identificación del branch práctica 3		
 	//Usamos dos variables, 'numero' para guardar el número, y 'resultado' para guardar el resultado.
 	float numero, resultado;
 	//Se le pide al usuario introduzca un número cualquiera.
@@ -21,6 +22,9 @@ int main(int argc, char *argv[]) {
 	//Se realiza el proceso para resolver la ecuación usando el orden de prioridades (paréntesis del más interno al más externo)
 	resultado = ((numero - 1)/4) - ((numero - 5)/36);
 	//Se imprime el resultado en pantalla
-	printf("\n El resultado de la primera operacion ((x-1)/4)-((x-5)/36) es: %f.", resultado);
+	printf("\n El resultado de la primera operacion ((x-1)/4)-((x-5)/36) es: %f.\n", resultado);
+	//Se realiza el proceso para resolver la segunda ecuación ((x + 1) / 8)-((2 * x - 3) / 16) * 6 usando el orden de prioridades (paréntesis del más interno al más externo)
+	resultado = ((numero + 1) / 8) - ((2 * numero - 3) / 16) * 6;
+	printf(" El resultado de la segunda operacion ((x + 1) / 8)-((2 * x - 3) / 16) * 6 es %f\n", resultado);
 	return 0;
 }
