e6e4338cfa9a3fa7464a55bd84ad4a5a44b771a7
Fernando Navarro <kevin.navarrogrz@gmail.com>
2019-08-25 02:39:42 -0500

    Agregado del programa para procesar la primera ecuación.

--- main.c
+++ main.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/*
+Autor: Kevin Fernando Navarro Garza
+Fecha: 25/08/2019
+Grupo: 008
+Horario: Sábados 09:00 - 11:00
+Matrícula: 1618122
+*/
 
 int main(int argc, char *argv[]) {
 	//Identificación del branch práctica 3
+	//Usamos dos variables, 'numero' para guardar el número, y 'resultado' para guardar el resultado.
+	float numero, resultado;
+	//Se le pide al usuario introduzca un número cualquiera.
+	printf(" Introduzca un numero: ");
+	//Se guarda el número dentro de la variable numero
+	scanf("%f", &numero);
+	//Se muestra el numero introducido
+	printf("Ha introducido el número %f.\n", numero);
+	//Se realiza el proceso para resolver la ecuación usando el orden de prioridades (paréntesis del más interno al más externo)
+	resultado = ((numero - 1)/4) - ((numero - 5)/36);
+	//Se imprime el resultado en pantalla
+	printf("\n El resultado de la primera operacion ((x-1)/4)-((x-5)/36) es: %f.", resultado);
 	return 0;
 }
