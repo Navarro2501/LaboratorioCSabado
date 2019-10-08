1079de0f1a4dbdd3beef5412f7f5b17900d3ce1d
Fernando Navarro <kevin.navarrogrz@gmail.com>
2019-10-08 14:04:07 -0500

    Practica 9

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
+/*
+Autor: Kevin Fernando Navarro
+Fecha: 10/07/19
+Grupo: 008
+Horario: Sabados 09:00 - 11:00
+Matricula: 1618122
+*/
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main(void) {
+  int ventasEmpleadoOficinaEdificio[6][10][2];
+  ventasEmpleadoOficinaEdificio[2][6][1] = 4234;
+  printf("El empleado numero 3 de la oficina numero 7 del edificio número 2 ha vendido %d unidades.\n\n", ventasEmpleadoOficinaEdificio[2][6][1]);
 
-int main(int argc, char *argv[]) {
-	return 0;
+  //El Array esta compuesto por 120 elementos
+  //En dado caso se muestra un error ya que tal direccion no existe
+
+  ventasEmpleadoOficinaEdificio[2][6][1] = 4234 + 10;
+  printf("El empleado numero 3 de la oficina numero 7 del edificio número 2 ha vendido %d unidades.\n", ventasEmpleadoOficinaEdificio[2][6][1]);
+  return 0;
 }
